7fe236cb314899c45d4c5e6b0845139195360031
Alvadda <pansch.c@t-online.de>
2024-03-29 19:35:49 +0100

    add cli for adding new applications

---
+++ apps/app-tracker/cli.mjs
@@ -0,0 +1,64 @@
+import activeWin from 'active-win'
+import axios from 'axios'
+import inquirer from 'inquirer'
+
+const mapWindow = (windows) =>
+  windows.map((w) => ({
+    processName: w.owner.name,
+    title: w.title,
+  }))
+
+const addTrackingApplication = async (processName, titleDetail, color) => {
+  const { data } = await axios.post('http://localhost:3000/api/application', {
+    processName,
+    titleDetail,
+    color,
+  })
+
+  console.log('Successfully Added: ', data)
+}
+
+const runCLI = async () => {
+  const openWindows = mapWindow(activeWin.getOpenWindowsSync())
+
+  const questions = [
+    {
+      type: 'list',
+      name: 'selectedWindow',
+      message: 'Select a window to track:',
+      choices: openWindows.map((item) => ({
+        name: item.processName,
+        value: item,
+      })),
+    },
+    {
+      type: 'input',
+      name: 'titleDetail',
+      message: (answers) => `You can enter a title selector to track specific state of the Application: 
+      ${answers.selectedWindow.title}
+      :`,
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter a hex color code:',
+    },
+    {
+      type: 'confirm',
+      name: 'confirm',
+      message: (answers) => `is your data correct? 
+      Application: ${answers.selectedWindow.processName}
+      Title Detail: ${answers.titleDetail}
+      Color: ${answers.color}
+      :`,
+      default: false,
+    },
+  ]
+
+  const answers = await inquirer.prompt(questions)
+  if (!answers.confirm) return
+
+  await addTrackingApplication(answers.selectedWindow.processName, answers.titleDetail, answers.color)
+}
+
+runCLI().catch(console.error)
